aebcd83e48f0e71d2f59c801e82dce37c349339f
Mayank <mayankchutani.14@gmail.com>
2017-09-18 18:53:57 -0500

    added mapReduce func

--- src/main/scala/week2/session2.scala
+++ src/main/scala/week2/session2.scala
@@ -3,9 +3,7 @@ object session2 extends App {
     if (a > b) 0
     else f(a) + sum(f, a+1, b)
 
-  def sumTailRecursive(f: Int => Int)(a: Int, b: Int): Int = {
-    def loop(acc: Int, x: Int, y: Int): Int = {
-      if (x > y) acc
+  def sumTailRecursive(f: Int => Int)(a: Int, b: Int): Int = { def loop(acc: Int, x: Int, y: Int): Int = { if (x > y) acc
       else {
         loop(acc + f(x), x + 1, y)
       }
@@ -15,4 +13,10 @@ object session2 extends App {
   println(sumTailRecursive(x => x * x)(1, 4))
 
     println(sum(x => x * x, 1, 4))
+
+    def mapReduce(f: Int => Int)(combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int= {
+      if (a > b) zero
+      else combine(f(a), mapReduce(f)(combine, zero)(a+1, b))
+    }
+    println(mapReduce(x => x * x)((x, y) => x + y, 0)(1, 4))
 }
