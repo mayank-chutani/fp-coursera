6496d94c871f0faa86cf7885bc8f3e2e2943c9a2
Mayank <mayankchutani.14@gmail.com>
2017-09-18 18:27:36 -0500

    Revert "added tail recursive sum"
    This reverts commit 73d090b36af57e97aec72206d506b8c58fb42bb4.

--- src/main/scala/week2/session2.scala
+++ src/main/scala/week2/session2.scala
@@ -3,17 +3,6 @@ object session2 extends App {
     if (a > b) 0
     else f(a) + sum(f, a+1, b)
 
-  def sumTailRecursive(f: Int => Int, a: Int, b: Int): Int = {
-    def loop(acc: Int, x: Int, y: Int): Int = {
-      if (x > y) acc
-      else {
-        loop(acc + f(x), x + 1, y)
-      }
-    }
-    loop(0, a, b)
-  }
-  println(sumTailRecursive(x => x * x, 1, 4))
-
     println(sum(x => x * x, 1, 4))
 }
 
