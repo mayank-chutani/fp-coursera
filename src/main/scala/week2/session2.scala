a740fc95a628b07184cb181fc1aa4282be85afab
Mayank <mayankchutani.14@gmail.com>
2017-09-18 18:34:42 -0500

    added currying

--- src/main/scala/week2/session2.scala
+++ src/main/scala/week2/session2.scala
@@ -3,7 +3,7 @@ object session2 extends App {
     if (a > b) 0
     else f(a) + sum(f, a+1, b)
 
-  def sumTailRecursive(f: Int => Int, a: Int, b: Int): Int = {
+  def sumTailRecursive(f: Int => Int)(a: Int, b: Int): Int = {
     def loop(acc: Int, x: Int, y: Int): Int = {
       if (x > y) acc
       else {
@@ -12,7 +12,7 @@ object session2 extends App {
     }
     loop(0, a, b)
   }
-  println(sumTailRecursive(x => x * x, 1, 4))
+  println(sumTailRecursive(x => x * x)(1, 4))
 
     println(sum(x => x * x, 1, 4))
 }
