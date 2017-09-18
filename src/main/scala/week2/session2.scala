4fc07cb45369cfa527a4292bffd52664c8e2eee6
Mayank <mayankchutani.14@gmail.com>
2017-09-18 18:29:30 -0500

    a

--- src/main/scala/week2/session2.scala
+++ src/main/scala/week2/session2.scala
@@ -1,8 +1,18 @@
 object session2 extends App {
-  def sum(f: Int => Int, a: Int, b: Int): Int = 
-    if (a > b) 0
-    else f(a) + sum(f, a+1, b)
+    def sum(f: Int => Int, a: Int, b: Int): Int = 
+          if (a > b) 0
+        else f(a) + sum(f, a+1, b)
 
-    println(sum(x => x * x, 1, 4))
+          def sumTailRecursive(f: Int => Int, a: Int, b: Int): Int = {
+                def loop(acc: Int, x: Int, y: Int): Int = {
+                        if (x > y) acc
+                              else {
+                                        loop(acc + f(x), x + 1, y)
+                                              }
+                                                  }
+                    loop(0, a, b)
+                      }
+            println(sumTailRecursive(x => x * x, 1, 4))
+
+                println(sum(x => x * x, 1, 4))
 }
-
