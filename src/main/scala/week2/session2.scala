c356da6eefe170c9a62667e60035fafd03e1c63a
mchutani-CS <mchutani@cognitivescale.com>
2017-09-20 11:35:57 -0500

    added session 3

--- src/main/scala/week2/session2.scala
+++ src/main/scala/week2/session2.scala
@@ -19,4 +19,30 @@ object session2 extends App {
       else combine(f(a), mapReduce(f)(combine, zero)(a+1, b))
     }
     println(mapReduce(x => x * x)((x, y) => x + y, 0)(1, 4))
+
+  def a = new Rational(12, 30)
+  println(a.toString())
+  println(a.less(new Rational(1, 0)))
+  println(-a)
 }
+
+class Rational(x: Int, y: Int) {
+  def this(x:Int) = this(x, 1)
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  private val g = gcd(x, y)
+  def numerator = x
+  def denominator = y
+
+  override def toString() = {
+    this.numerator / g + "/" + this.denominator/g
+  }
+  def less(that: Rational) = numerator * that.denominator < that.numerator * denominator
+  def max(that: Rational) = if (this.less(that)) that else this
+
+  def + (that: Rational) = {
+    new Rational(numerator + that.numerator / denominator + that.denominator)
+  }
+
+  def unary_- : Rational = new Rational(-numerator, denominator)
+}
+
